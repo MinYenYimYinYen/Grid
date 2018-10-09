efde6ce4f8f817d05f65266f748efe08228bcdb8
MinYenYimYinYen <lukeholker@gmlawncare.onmicrosoft.com>
2018-10-09 16:56:15 -0500

    SADB, Territory, Block

---
+++ Grid/Models/Block.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid.Models
+{
+	public class Block
+	{
+		public Location TopLeft { get; set; }
+		public Location BottomRight { get; set; }
+
+		private ObservableCollection<Location> content;
+		public ObservableCollection<Location> Content
+		{
+			get
+			{
+				if (content == null) content = new ObservableCollection<Location>();
+				return content;
+			}
+		}
+
+
+		public void Fill(List<Location> locations, bool ClearExisting)
+		{
+			if (ClearExisting) this.Content.Clear();
+			foreach (var loc in locations)
+			{
+				this.Content.Add(loc);
+			}
+		}
+	}
+}
---
+++ Grid/Models/Territory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid.Models
+{
+	public class Territory
+	{
+		public Location TopLeft { get; set; }
+		public Location BottomRight { get; set; }
+
+		public int DivLat { get; set; }
+		public int DivLon { get; set; }
+
+		public ObservableCollection<Block> Blocks;
+
+	}
+}
