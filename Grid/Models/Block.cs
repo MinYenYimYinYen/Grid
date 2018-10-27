c95d7e62229a69b2985e150703441c856ea78d74
MinYenYimYinYen <lukeholker@gmlawncare.onmicrosoft.com>
2018-10-27 00:28:40 -0500

    Simplified Blocks

--- Grid/Models/Block.cs
+++ Grid/Models/Block.cs
@@ -12,25 +12,5 @@ namespace Grid.Models
 	{
 		public Location TopLeft { get; set; }
 		public Location BottomRight { get; set; }
-
-		private ObservableCollection<Location> content;
-		public ObservableCollection<Location> Content
-		{
-			get
-			{
-				if (content == null) content = new ObservableCollection<Location>();
-				return content;
-			}
-		}
-
-
-		public void Fill(List<Location> locations, bool ClearExisting)
-		{
-			if (ClearExisting) this.Content.Clear();
-			foreach (var loc in locations)
-			{
-				this.Content.Add(loc);
-			}
-		}
 	}
 }
